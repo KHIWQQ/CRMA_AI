f2759d4a98843f7a40f634941f694183893c5034
KHIWQQ <eiq1627@gmail.com>
2022-12-11 19:15:20 +0700

    KNN edit

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,9 +1,4 @@
-# import numpy
 import pandas as pd
-# import keras.models
-# import sequential
-# import keras. layers
-# # import dense
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,24 +7,31 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 
 # load the dataset
-dataset = pd.read_csv('pima-indians-diabetes', detimiter=',')
+dataset = np.loadtxt('ABC.csv', delimiter=',')
+# dataset = pd.read_csv('Test.csv')
 # split into input (X) and output (y) variables
-X = dataset[: ,0:8]
-y = dataset[: ,8]
-print (X)
-print(y)
+# X = dataset
+# Y = dataset
+X = dataset[:,0:8]
+Y = dataset[:,8]
+print(X)
+print(Y)
+
 # define the keras model
-model = sequential()
-model.add(dense(12, input_din=8, activation='relu'))
-model.add(dense(8, activation='relu'))
-model.add(dense(1, activation='sigmoid'))
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
 # compile the keras model
-model.compile (Loss='binary_crossentropy', optinizer='adam', netrics=['accuracy' ])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # fit the keras model on the dataset
-model.fit(X, y, epochs=250, batch_size=100)
+model.fit(X, Y, epochs=150, batch_size=100)
 # evaluate the keras model
-_, accuracy = model.evaluate (X, y)
+_, accuracy = model.evaluate(X, Y)
 # accuracy = model.evaluate(X. Yverbose=0) tin case we do not need to  print out
-print ('Accuracy: %.2f' % (accuracy*100))
+print('Accuracy: %.2f' % (accuracy*100))
 # make class predictions with the model
-predictions = model.predict_classes (X)
+predictions = model.predict(X)
+# round predictions
+rounded = [round(x[0]) for x in predictions]
---
+++ KNN/main.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+print("Tensorflow version: ", tf.__version__)
+
+# from tensorflow.keras.layers import Dense, Flatten, Conv2D
+# from tensorflow.keras import Model
